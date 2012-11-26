28f964f1d162b0da04ebd80a76158a18fb7093c2
niceilm <niceilm@naver.com>
2012-11-26 10:46:05 +0900

    change crud

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
 	uploadPath : __dirname + '/uploads',
 	express : {
-		port : (process.env.VMC_APP_PORT || process.env.PORT || 3001),
-		host : (process.env.VCAP_APP_HOST || 'local.niceilm.net' || 'localhost')
+		port : (process.env.VMC_APP_PORT || process.env.PORT || 3000),
+		host : (process.env.VCAP_APP_HOST || 'localhost')
 	},
 	facebook : {
-		appId : '449290298463113',
-		appSecret : 'a3563a2fc4e40850e1af14702f9a5d1b'
+		appId : process.env.VCAP_APP_HOST ? '449290298463113' : '252233128235888',
+		appSecret : process.env.VCAP_APP_HOST ? 'a3563a2fc4e40850e1af14702f9a5d1b' : '02b6e2784fa03a0a2bacb659008ed34a'
 	}
 };
