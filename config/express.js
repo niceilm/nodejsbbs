baf6ad49e1dc2c34a82c89b6e9a036f188c60da7
niceilm <niceilm@naver.com>
2012-12-04 19:23:22 +0900

    jquery mobile 적용

--- config/express.js
+++ config/express.js
@@ -2,8 +2,9 @@
  * express 환경
  * @type {Object}
  */
+var path = require('path');
 module.exports = {
-	uploadPath : __dirname + '/../uploads',
+	uploadPath : path.normalize(__dirname + '/../uploads'),
 	port : (process.env.VMC_APP_PORT || process.env.PORT || 3000),
 	host : (process.env.VCAP_APP_HOST || 'localhost')
 };
